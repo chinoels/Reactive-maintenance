5a990765b79617a3ac2e189c17d82f394be1d262
chinoels <129370619+chinoels@users.noreply.github.com>
2025-07-18 23:12:54 +1200

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Reactive Maintenance</h1>
+      <p>This is your QR-based property maintenance portal.</p>
+    </div>
+  );
+}
